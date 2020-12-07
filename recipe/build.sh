83d32f81516a3550e3f196df575d6ecce2f4ef9f
Uwe L. Korn <uwe.korn@quantco.com>
2020-12-07 16:54:43 +0100

    Back to yarn again

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,9 @@ npm install -g npm@latest
 
 mkdir -p $PREFIX/share/vega-lite-cli
 pushd $PREFIX/share/vega-lite-cli
-npm install --save vega-lite@$PKG_VERSION
+yarn global add install-peerdeps
+yarn add vega-lite@$PKG_VERSION
+
 
 pushd $PREFIX/bin
 for cmd in vl2pdf vl2png vl2svg vl2vg
